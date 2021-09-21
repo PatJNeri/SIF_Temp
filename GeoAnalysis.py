6c82e36a9a39ecff69efbb7ed5124f0f4c42cf9b
Patrick <pjneri@email.arizona.edu>
2021-09-21 14:05:21 -0700

    9-21 school side
    Noticed and started to work thru the issues with some listed lat and lon being incorrect or not plotting correctly. Want to ensure that this works, as making this iterative means when I add other lat and lon to the excel it will still run itself. Need to begin considering if I wish to use a different method for getting the data I want out of the system (assuming I can find it...)

--- GeoAnalysis.py
+++ GeoAnalysis.py
@@ -4,6 +4,7 @@
 # Patrick Neri
 # %%
 # Currently running on hastools2
+from matplotlib import transforms
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -33,8 +34,47 @@ PSIICGrass = PSIIContr[PSIIContr['type'] == 'grass-like']
 PSIICGrass.name = 'PSII control Grass-like'
 PSIICShrub = PSIIContr[PSIIContr['type'] == 'shrub']
 PSIICShrub.name = 'PSII control shrub'
+
+PSIIContr['Adjusted PFT'] = PSIIContr['PFT #']
+for i in range(len(PSIIContr['phiPSIImax'])):
+    if PSIIContr['PFT #'].iloc[i] == 71:
+        PSIIContr['Adjusted PFT'].iloc[i] = 14
+    if PSIIContr['PFT #'].iloc[i] == 73:
+        PSIIContr['Adjusted PFT'].iloc[i] = 14
+    if PSIIContr['PFT #'].iloc[i] == 19:
+        PSIIContr['Adjusted PFT'].iloc[i] = 15
+    if PSIIContr['PFT #'].iloc[i] == 23:
+        PSIIContr['Adjusted PFT'].iloc[i] = 15
+    if PSIIContr['PFT #'].iloc[i] == 61:
+        PSIIContr['Adjusted PFT'].iloc[i] = 15
+    if PSIIContr['PFT #'].iloc[i] == 41:
+        PSIIContr['Adjusted PFT'].iloc[i] = 15
+    if PSIIContr['PFT #'].iloc[i] == 17:
+        PSIIContr['Adjusted PFT'].iloc[i] = 16
+    if PSIIContr['PFT #'].iloc[i] == 51:
+        PSIIContr['Adjusted PFT'].iloc[i] = 16
+    if PSIIContr['PFT #'].iloc[i] == 63:
+        PSIIContr['Adjusted PFT'].iloc[i] = 16
+    if PSIIContr['PFT #'].iloc[i] == 39:
+        PSIIContr['Adjusted PFT'].iloc[i] = 4
+    if PSIIContr['PFT #'].iloc[i] == 53:
+        PSIIContr['Adjusted PFT'].iloc[i] = 4
+
+PSIIContr['timetime'] = PSIIContr['Time (h)']
+for i in range(len(PSIIContr['Time (h)'])):
+    if PSIIContr['Time (h)'].iloc[i] == 0:
+        PSIIContr['timetime'].iloc[i] = 0
+    elif ((PSIIContr['Time (h)'].iloc[i] > 0) & (PSIIContr['Time (h)'].iloc[i] < 48.1)):
+        PSIIContr['timetime'].iloc[i] = 1
+    elif ((PSIIContr['Time (h)'].iloc[i] > 48) & (PSIIContr['Time (h)'].iloc[i] < 336.1)):
+        PSIIContr['timetime'].iloc[i] = 2
+    elif PSIIContr['Time (h)'].iloc[i] > 336:
+        PSIIContr['timetime'].iloc[i] = 3
 # %%
-PSIIGEO = PSIIContr[PSIIContr['GEO'].notna() == True]
+# drop bad data options
+NewSet = PSIIContr.drop([250,251,252,253,1502,1503,1504,1505,1506,1507,1508,1509,1510,1511], axis=0)
+PSIIGEO = NewSet[NewSet['GEO'].notna() == True]
+print(np.unique(PSIIGEO['paper']))
 # %%
 # Here we need to formalize and split 'GEO' values to lat and lon
 PSIIGEO['lat_split'] = PSIIGEO['GEO']
@@ -54,17 +94,35 @@ for i in range(0, len(PSIIGEO['GEO'])):
     setnums = re.split('[°\'"]', PSIIGEO['lat_split'].iloc[i])
     print(setnums)
     if len(setnums) == 2:
-        PSIIGEO['lat_num'].iloc[i] == (float(setnums[0]) * (-1 if setnums[1] in ['W', 'S'] else 1))
+        PSIIGEO['lat_num'].iloc[i] == (float(setnums[0])) * (-1 if setnums[1] in ['W', 'S', ' W', ' S'] else 1)
     elif len(setnums) == 3:
-        PSIIGEO['lat_num'].iloc[i] = (float(setnums[0]) + float(setnums[1])/60) * (-1 if setnums[2] in ['W', 'S'] else 1)
+        PSIIGEO['lat_num'].iloc[i] = (float(setnums[0]) + float(setnums[1])/60) * (-1 if setnums[2] in ['W', 'S', ' W', ' S'] else 1)
     elif len(setnums) == 4:
-        PSIIGEO['lat_num'].iloc[i] = (float(setnums[0]) + float(setnums[1])/60 + float(setnums[2])/(60*60)) * (-1 if setnums[3] in ['W', 'S'] else 1)
+        PSIIGEO['lat_num'].iloc[i] = (float(setnums[0]) + float(setnums[1])/60 + float(setnums[2])/(60*60)) * (-1 if setnums[3] in ['W', 'S', ' W', ' S'] else 1)
+
+PSIIGEO['lon_num'] = PSIIGEO['lon_split']
+for i in range(0, len(PSIIGEO['GEO'])):
+    print(i)
+    setnums = re.split('[°\'"]', PSIIGEO['lon_split'].iloc[i])
+    print(setnums)
+    if len(setnums) == 2:
+        PSIIGEO['lon_num'].iloc[i] == (float(setnums[0])) * (-1 if setnums[1] in ['W', 'S', ' W', ' S'] else 1)
+    elif len(setnums) == 3:
+        PSIIGEO['lon_num'].iloc[i] = (float(setnums[0]) + float(setnums[1])/60) * (-1 if setnums[2] in ['W', 'S', ' W', ' S'] else 1)
+    elif len(setnums) == 4:
+        PSIIGEO['lon_num'].iloc[i] = (float(setnums[0]) + float(setnums[1])/60 + float(setnums[2])/(60*60)) * (-1 if setnums[3] in ['W', 'S', ' W', ' S'] else 1)        
 # %%
 # Here we need to plot them
+
 ax = plt.axes(projection=ccrs.PlateCarree())
+for i in range(0, len(PSIIGEO['GEO'])):
+    print(i)
+    ax.plot(PSIIGEO['lon_num'].iloc[i], PSIIGEO['lat_num'].iloc[i], 'bo', markersize=2)
+#    ax.text(float(PSIIGEO['lon_num'].iloc[i]) + 0.01, float(PSIIGEO['lat_num'].iloc[i]) - 0.01, PSIIGEO['paper'].iloc[i], transform=ccrs.PlateCarree())
+
+#ax.coastlines(color='black')
 ax.stock_img()
-
-
 plt.show()
 # %%
-deg, minutes, seconds, direction
+np.unique(PSIIGEO[''])
+# %%
